79757281e174e92bf02f03a36fbbd2d988b51035
Kumara Krishnan <5687650+Kumara-Krishnan@users.noreply.github.com>
2026-01-17 12:51:57 +0530

    refactor: improve project structure and logging setup in MainWindow

--- Bookmarkly.App/MainWindow.xaml.cs
+++ Bookmarkly.App/MainWindow.xaml.cs
@@ -1,3 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Cyclotron.Telemetry.DependencyInjection;
+using Cyclotron.Telemetry.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -5,14 +15,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -37,6 +43,28 @@ namespace Bookmarkly.App
             datas.Add(1);
             datas.Add(1);
             InitializeComponent();
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            var services = new ServiceCollection();
+
+            services.AddCyclotronTelemetry(options =>
+            {
+                options.ServiceName = "Bookmarkly";
+                options.ServiceVersion = "1.0.0";
+                options.DefaultModule = "main";
+
+                options.Logging.MinimumLevel = LogLevel.Debug;
+                options.Logging.File.Path = "{LocalAppData}/Bookmarkly/logs/app-.log";
+                options.Logging.File.RetainedFileCountLimit = 3; // Already set as default
+                options.Logging.File.RollingInterval = Serilog.RollingInterval.Day;
+            });
+
+            var provider = services.BuildServiceProvider();
+            var logger = provider.GetRequiredService<ICyclotronLogger>().ForModule("Instapaper");
+
+            logger.LogInformation("Starting operation"); // Caller info auto-captured!
         }
     }
 }
